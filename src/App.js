86ef3989b5fc334f026c8a25f168f69bc60a870d
farwaMuhibzada <farwafarid2017@gmail.com>
2024-08-05 14:31:23 +0430

    fixed linters error.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { useState } from "react";
-import quotes from "./assets/quotes.json";
-import { FaTwitter, FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
-import "./App.css";
+import { useState } from 'react';
+import { FaTwitter, FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
+import quotes from './assets/quotes.json';
+import './App.css';
 
-const getRandomQuote = () => {
-  return quotes[Math.floor(Math.random() * quotes.length)];
-};
+const getRandomQuote = () => quotes[Math.floor(Math.random() * quotes.length)];
 
 const getRandomColor = () => {
   const red = Math.floor(Math.random() * 128);
@@ -15,7 +13,7 @@ const getRandomColor = () => {
   return `rgb(${red}, ${green}, ${blue})`;
 };
 
-const transition = "all 1s";
+const transition = 'all 1s';
 
 function App() {
   const [quote, setQuote] = useState(getRandomQuote());
@@ -28,35 +26,35 @@ function App() {
 
   return (
     <div
-      className="background"
+      className='background'
       style={{ backgroundColor: randomColor, transition }}
     >
-      <div id="quote-box">
+      <div id='quote-box'>
         <div
-          className="quote-content"
+          className='quote-content'
           style={{ color: randomColor, transition }}
         >
-          <h2 id="text">
-            <FaQuoteLeft size="30" style={{ marginRight: "10px" }} />
+          <h2 id='text'>
+            <FaQuoteLeft size='30' style={{ marginRight: '10px' }} />
             {quote.quote}
-            <FaQuoteRight size="30" style={{ marginLeft: "10px" }} />
+            <FaQuoteRight size='30' style={{ marginLeft: '10px' }} />
           </h2>
-          <h4 id="author">- {quote.author}</h4>
+          <h4 id='author'>{quote.author}</h4>
         </div>
-        <div className="buttons">
+        <div className='buttons'>
           <a
             href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quote.quote}`}
-            id="tweet-quote"
+            id='tweet-quote'
             style={{
               backgroundColor: randomColor,
-              marginRight: "10px",
+              marginRight: '10px',
               transition,
             }}
           >
-            <FaTwitter color="white" />
+            <FaTwitter color='white' />
           </a>
           <button
-            id="new-quote"
+            id='new-quote'
             onClick={changeQuote}
             style={{ backgroundColor: randomColor, transition }}
           >
