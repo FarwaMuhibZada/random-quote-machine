ca3f5670e14b32b40dfd7d3a789c0166af863beb
farwaMuhibzada <farwafarid2017@gmail.com>
2024-08-05 19:33:39 +0430

    added features in readme.md file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ const getRandomColor = () => {
   const red = Math.floor(Math.random() * 128);
   const green = Math.floor(Math.random() * 128);
   const blue = Math.floor(Math.random() * 128);
-
   return `rgb(${red}, ${green}, ${blue})`;
 };
 
